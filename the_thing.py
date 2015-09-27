a8d980fdbb08e742a59b67a6f4381dff0b16d300
kapteinstein <eriklio@stud.ntnu.no>
2015-09-28 00:49:04 +0200

    må fikse:  - potensialet  - OPTIMALISERING

--- the_thing.py
+++ the_thing.py
@@ -17,52 +17,37 @@ pi = np.pi  # gode gamle pi = 3.1415...
 
 '''
 
-def Psi_initial(x, x0, t):
-    C = 1
-    w = 1
-    sigma_x = 2
-
-    psi_r = C * np.exp(-(x - x0)**2 / (2*sigma_x**2)) * (np.cos(k*x - w*t))
-    psi_i = C * np.exp(-(x - x0)**2 / (2*sigma_x**2)) * (np.sin(k*x - w*t))
+def Psi(x, x0, w, C, sigma_x, t):
+    psi_r = C * np.exp(-(x - x0)**2 / (2*sigma_x**2)) * ( np.cos(k*x - w*t))
+    psi_i = C * np.exp(-(x - x0)**2 / (2*sigma_x**2)) * ( np.sin(k*x - w*t))
     psi_r[0] = 0
     psi_r[-1] = 0
     psi_i[0] = 0
     psi_i[-1] = 0
     return psi_r + 1j*psi_i
 
-'''
-def Psi_dt(a, dt):
-    for n in range(np.size(a) - 2):
-        n = n + 1
-        a.imag[n] = a.imag[n-1] - dt * ( V[n] * a.real[n] - (1/(2*m*(dx**2))) *
-                                         (a.real[n+1] - 2*a.real[n] + a.real[n-1]))
 
-        a.real[n] = a.real[n-1] + dt * ( V[n] * a.imag[n] - (1/(2*m*(dx**2)))*
-                                         (a.imag[n+1] - 2*a.imag[n] + a.imag[n-1]))
-    return a
-'''
 
-def Psi_dt(a, dt):
-    for n in range(np.size(a) - 2):
-        n += 1
-        a.imag[n] -= dt * (V[n] * a.real[n] - (1 / (2 * m * (dx ** 2))) *
-                           (a.real[n + 1] - 2 * a.real[n] + a.real[n - 1]))
+def Psi_dt(a, dt, number_of_times):
+    for i in range(number_of_times):
+        for n in range(np.size(a) - 2):
+            n += 1
+            a.imag[n] -= dt * (V[n] * a.real[n] - (1 / (2 * m * (dx ** 2))) *
+                               (a.real[n + 1] - 2 * a.real[n] + a.real[n - 1]))
 
-        a.real[n] += dt * (V[n] * a.imag[n] - (1 / (2 * m * (dx ** 2))) *
-                           (a.imag[n + 1] - 2 * a.imag[n] + a.imag[n - 1]))
+            a.real[n] += dt * (V[n] * a.imag[n] - (1 / (2 * m * (dx ** 2))) *
+                               (a.imag[n + 1] - 2 * a.imag[n] + a.imag[n - 1]))
     return a
 
 
-'''
+
 def potential(a):
     b = np.zeros(np.size(a))
     for n in range(np.size(a) - 1):
-        if n <= 10:
-            b[n] = 0
-        else:
-            b[n] = 100
+        if (L/2 - l/2) < a[n] < (L/2 + l/2):
+            b[n] = 1/2 * E
     return b
-'''
+
 
 
 def plotter(X, a):  # definer plottefunksjonen.
@@ -83,12 +68,16 @@ L = 20  #
 # w = ...  # omega
 # E = ...  # energy
 # C = ...  # normalization constant
-
+C = 1
+sigma_x = 1
+w = 1
+l = L/50
+E = h*w
 
 plt.ion() # må ha med denne for å kunne modifisere plottet etter at det er tegnet opp.
 
 # definer området denne shiten skal virke over, eg. fra x0 -> x1
-N = 200
+N = 500
 x0 = 5
 x1 = L
 t = 0
@@ -102,14 +91,13 @@ print('dt: ', dt)
 # initsialisere
 X = np.linspace(0, x1, num=N)
 # V = potential(X)  # midlertidig potential
-V = X*0
-psi = Psi_initial(X, x0, t)  # går sikkert ann å gjøre denne penere eller på en bedre måte.
+V = potential(X)
+psi = Psi(X, x0, w, C, sigma_x, t)  # går sikkert ann å gjøre denne penere eller på en bedre måte.
 
 
 plotter(X, psi)  # bare midlertidig for å se at ting funker.
 
-'''
+
 for n in range(10):
-    psi = Psi_dt(psi, dt)
+    psi = Psi_dt(psi, dt, 500)
     plotter(X, psi)
-'''
