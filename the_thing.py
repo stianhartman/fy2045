83dabb2b5684a032cf6cd3154989e5c898cae54e
kapteinstein <eriklio@stud.ntnu.no>
2015-09-27 18:00:29 +0200

    oppdatert variabler

--- the_thing.py
+++ the_thing.py
@@ -4,27 +4,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# definer variabler, funksjoner, etc.
-
-h = 1.055 * 10 ** (-34)  # h-bar in J*s
 pi = np.pi  # gode gamle pi = 3.1415...
-k = 2 * pi  # wavenumber
-# w = ...  # omega
-# plt.ion() # må ha med denne for å kunne modifisere plottet etter at det er tegnet opp.
 
 '''
 # wave function = wave packet:
-# psi = Ae^(-bx^2)*e^i(kx-wt)
-#     = Ae^(-bx^2)*(cos(kx-wt) - i*sin(kx-wt))    # hva er A og b??
-# fra wikipedia ser vi:
-# psi(x,t) = e^(-(x-ct))^2 * ( cos(k * (x-ct)) +i*sin(k * (x-ct)))  # vet ikke helt hva c er. c muligens = omega
-
+# psi = Ce^(-(x-x0)^2/(2*sigma_x**2))*e^i(kx-wt)
+#     = Ce^(-(x-x0)^2/(2*sigma_x**2))*(cos(kx-wt) + i*sin(kx-wt))    # hva er C ??
+#
 # psi = psi_r + i * psi_i
-# psi_r = e^(-(x-ct))^2 * ( cos(k * (x-ct)) )
-# psi_i = e^(-(x-ct))^2 * ( sin(k * (x-ct)) )
+# psi_r = Ce^(-(x-x0)^2/(2*sigma_x**2)) * ( cos(kx-wt) )
+# psi_i = Ce^(-(x-x0)^2/(2*sigma_x**2)) * ( sin(kx-wt) )
 
 '''
+# def Psi(x, t):
 
 
 def Psi_r(x, t):
@@ -41,7 +33,7 @@ def Psi_i(x, t):
 
 '''
 def V(x):
-    return ......
+    return
 '''
 
 
@@ -53,11 +45,23 @@ def plotter(X, psi_real, psi_imag):  # definer plottefunksjonen.
     plt.show()
 
 
+# definer variabler, funksjoner, etc.
+
+h = 1  # h-bar
+m = 1  # mass, m
+k = 20  # wavenumber
+L = 20  #
+# w = ...  # omega
+# E = ...  # energy
+
+
+# plt.ion() # må ha med denne for å kunne modifisere plottet etter at det er tegnet opp.
+
 # definer området denne shiten skal virke over, eg. fra x0 -> x1
 N = 1000
-x0 = - 2*pi
-x1 = 2*pi
-t = 0
+x0 = 0
+x1 = L
+t0 = 0
 
 X = np.linspace(x0, x1, num=N)
 psi_r = Psi_r(X, t)
