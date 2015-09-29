5ba5e12728dd6cfb76493d4b12c94e3fbbdb6977
kapteinstein <eriklio@stud.ntnu.no>
2015-09-29 20:51:48 +0200

    import stuff

--- the_thing.py
+++ the_thing.py
@@ -2,8 +2,8 @@
 
 # import div
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
+#import matplotlib
+#matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
